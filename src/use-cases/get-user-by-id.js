ef55e709f9f60d9a9fcb731de3f7428d947440f2
Roger Santos <roger.santos36@gmail.com>
2024-10-22 20:09:54 -0300

    feat: add GetUserByIdController and validations

--- src/use-cases/get-user-by-id.js
+++ src/use-cases/get-user-by-id.js
@@ -1,9 +1,9 @@
-import { PostgresGetUserByIdRepository } from "../repositories/postgres/get-user-by-id";
+import { PostgresGetUserByIdRepository } from "../repositories/postgres/get-user-by-id.js";
 
 export class GetUserByIdUseCase {
   async execute(userId) {
     const getUserByIdRepository = new PostgresGetUserByIdRepository();
-    const user = getUserByIdRepository.execute(userId);
+    const user = await getUserByIdRepository.execute(userId);
     return user;
   }
 }
