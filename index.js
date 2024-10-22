d812973089ca7fa4fe2d8309707790a2cb04d79a
Roger Santos <roger.santos36@gmail.com>
2024-10-22 17:21:55 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 import "dotenv/config.js";
 import express from "express";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-import { PostgresHelper } from "./src/database/postgres/helper.js";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const results = await PostgresHelper.query("SELECT * FROM users;");
-  res.send(JSON.stringify(results));
-});
+app.use(express.json());
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
