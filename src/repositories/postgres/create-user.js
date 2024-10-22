0093ef6a3b36fcc2551045003a8c3bac5a0d2403
Roger Santos <roger.santos36@gmail.com>
2024-10-22 17:54:24 -0300

    feat: add CreateUserUseCase

--- src/repositories/postgres/create-user.js
+++ src/repositories/postgres/create-user.js
@@ -3,9 +3,8 @@ import { PostgresHelper } from "../../database/postgres/helper";
 export class PostgresCreateUserRepository {
   async execute(createUserParams) {
     const results = await PostgresHelper.query(
-      "INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)",
+      "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
       [
-        createUserParams.id,
         createUserParams.first_name,
         createUserParams.last_name,
         createUserParams.email,
---
+++ src/use-cases/create-user.js
@@ -0,0 +1,16 @@
+import bcrypt from "bcrypt";
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user";
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+    const user = {
+      ...createUserParams,
+      password: hashedPassword,
+    };
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+    const createdUser = postgresCreateUserRepository.execute(user);
+    return createdUser;
+  }
+}
